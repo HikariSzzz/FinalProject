a6d89da27040bde6dd2e9b8c4b5a9198115c4c13
varrel <varellnashwaalifiowijaya@gmail.com>
2025-10-06 12:30:18 +0700

    Nyoba tambah baris

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi , ini kode baris pertama");
+        System.out.println("Ini kode baris kedua");
     }
 }
