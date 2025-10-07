9e3edf71c34944c77a0fb765378b5b1b36fdcf5e
varrel <varellnashwaalifiowijaya@gmail.com>
2025-10-07 15:52:36 +0700

    Tugas 1

--- ifCetakKRS29.java
+++ ifCetakKRS29.java
@@ -4,7 +4,7 @@ public class ifCetakKRS29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("--- Cetak KRS SIAKAD --- ");
-        System.out.println("Apakah UKT sudah luas? (true/false) ");
+        System.out.println("Apakah UKT sudah lunas? (true/false) ");
         boolean uktlunas = sc.nextBoolean();
 
         if (uktlunas) {
@@ -12,5 +12,11 @@ public class ifCetakKRS29 {
         } else {
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
         }
+
+        // 🔽 Tambahan baris baru: versi Ternary Operator
+        String hasil = (uktlunas)
+            ? "Pembayaran UKT terverifikasi"
+            : "Silahkan cetak KRS dan minta tanda tangan DPA";
+        System.out.println(hasil);
     }
 }
