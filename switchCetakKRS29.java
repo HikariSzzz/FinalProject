221f5cb24abf7d58abe0fb2c045fe35c61423161
varrel <varellnashwaalifiowijaya@gmail.com>
2025-10-07 14:56:41 +0700

    Percobaan 2

---
+++ switchCetakKRS29.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class switchCetakKRS29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("--- Cetak KRS SIAKAD --- ");
+        System.out.println("Masukkan Semester Saat Ini");
+        int semester = sc.nextInt();
+        switch (semester) {
+            case 1:
+            System.out.println("KRS Semester 1 ditampilan");
+                break;
+            case 2:
+            System.out.println("KRS Semester 2 ditampilan");
+                break;
+            case 3:
+            System.out.println("KRS Semester 3 ditampilan");
+                break; 
+            case 4:
+            System.out.println("KRS Semester 4 ditampilan");
+                break;
+            case 5:
+            System.out.println("KRS Semester 5 ditampilan");    
+                break;
+            case 6:
+            System.out.println("KRS Semester 6 ditampilan");
+                break;
+            case 7:
+            System.out.println("KRS Semester 7 ditampilan");
+                break;
+            case 8:
+            System.out.println("KRS Semester 8 ditampilan");    
+                break;
+            default:
+            System.out.println("Semester tidak valid");                            
+        }
+        }
+ }
